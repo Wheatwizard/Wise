53984e7980932a3b251469956f910f805681e40b
wheatwizard <olivee3@rpi.edu>
2017-03-21 23:31:13 -0400

    added -f option

--- wise.py
+++ wise.py
@@ -13,6 +13,12 @@ if __name__ == "__main__":
 		help="Formats output as binary"
 	)
 	parser.add_option(
+		"-f",
+		"--file",
+		dest="filename",
+		help="File name for input arguments"
+	)
+	parser.add_option(
 		"-a",
 		"--ascii",
 		dest="OUTPUT",
@@ -26,7 +32,24 @@ if __name__ == "__main__":
 	except:
 		print "Please provide a valid file."
 	else:
-		interpreter = Interpreter(f.read(),options.OUTPUT,*map(int,args[1:]))
-		while interpreter.step():pass
-		print interpreter
-		f.close()
+		if options.filename:
+			try:
+				g = open(options.filename)
+			except:
+				print "There was an error opening %s"%(options.filename)
+			else:
+				try:
+					input = map(int,g.read().split())
+				except:
+					#TODO make more descriptive error
+					print "Error reading file %s"%(options.filename)
+				interpreter = Interpreter(f.read(),options.OUTPUT,*input)
+				g.close()
+				f.close()
+				while interpreter.step():pass
+				print interpreter
+		else:
+			interpreter = Interpreter(f.read(),options.OUTPUT,*map(int,args[1:]))
+			f.close()
+			while interpreter.step():pass
+			print interpreter
