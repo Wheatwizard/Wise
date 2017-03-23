ce88357031f5de8e32cc2458bc63097bc8f616a4
wheatwizard <olivee3@rpi.edu>
2017-03-23 16:27:36 -0400

    Actually fixed the [ problem

--- Interpreter.py
+++ Interpreter.py
@@ -1,49 +1,46 @@
+from Stack import Stack
+
 class Interpreter(object):
 	def __init__(self,source,output,*args):
-		self.stack = list(args)
+		self.stack = Stack(args)
 		self.source = source
 		self.scope = []
 		self.pointer = 0
 		self.output = output
-	def peak(self):
-		return not(self.stack) or self.stack[-1]
-	def pick(self):
-		if self.stack:
-			return self.stack.pop()
-		else: return 0
 	def step(self):
 		if self.pointer >= len(self.source):return False
 		command = self.source[self.pointer]
 		if command == "[":
-			if not self.peak:
+			if self.stack[-1]:
+				self.scope.append(self.pointer)
+			else:
+				#If the TOS is zero jump to the next ]
 				tempScope = 1
-				while tempScope and self.pointer <= len(self.source):
+				while tempScope and (self.pointer < len(self.source)):
 					self.pointer += 1
 					if self.source[self.pointer] == "]":
-						self.tempScope -= 1
+						tempScope -= 1
 					elif self.source[self.pointer] == "[":
-						self.tempScope += 1
-			else:
-				self.scope.append(self.pointer)
+						tempScope += 1
 		elif command == "]":
-			if self.peak():
+			if self.stack[-1]:
 				self.pointer = self.scope[-1]
 			else:
 				self.scope.pop()
 		elif command == "&":
-			self.stack.append(self.pick()&self.pick())
+			self.stack.append(self.stack.pop()&self.stack.pop())
 		elif command == "|":
-			self.stack.append(self.pick()|self.pick())
+			self.stack.append(self.stack.pop()|self.stack.pop())
 		elif command == "^":
-			self.stack.append(self.pick()^self.pick())
+			self.stack.append(self.stack.pop()^self.stack.pop())
 		elif command == "~":
-			self.stack.append(~self.pick())
+			self.stack.append(~self.stack.pop())
 		elif command == "-":
-			self.stack.append(-self.pick())
+			self.stack.append(-self.stack.pop())
 		elif command == "<":
-			self.stack.append(self.pick()<<1)
+			self.stack.append(self.stack.pop()<<1)
 		elif command == ">":
-			self.stack.append(self.pick()>>1)
+			self.stack.append(self.stack.pop()>>1)
 		elif command == ":":
 			if self.stack:
 				self.stack.append(self.stack[-1])
@@ -57,6 +54,7 @@ class Interpreter(object):
 				self.stack = self.stack[1:] + [self.stack[0]]
 		self.pointer += 1
 		return True
+
 	def __str__(self):
-		functions = {"i":str,"b":lambda x:"-"+bin(x)[3:] if x < 0 else bin(x)[2:],"a":lambda x:chr(x%256)}
-		return ("" if self.output == "a" else" ").join(map(functions[self.output],self.stack))
+		functions = {"i":str,"b":lambda x:"-"+bin(x)[3:] if x<0 else bin(x)[2:],"a":lambda x:chr(x%256)}
+		return (""if self.output=="a" else" ").join(map(functions[self.output],self.stack))
